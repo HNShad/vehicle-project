db1e32d6639ce931a48c760994bb350ce9b0f7a7
Hossein Nourozi Shad <hens.shad@gmail.com>
2019-07-16 00:17:48 +0200

    Fixing UI service module

--- ui-service/src/main/java/com/hens/practise/vehicle/uiservice/controller/VehicleUiController.java
+++ ui-service/src/main/java/com/hens/practise/vehicle/uiservice/controller/VehicleUiController.java
@@ -5,7 +5,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
